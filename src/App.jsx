ea9f31fd8bf43eeff8566a66c3de35795d7670e9
student3 <student3@itpark2.local>
2023-06-14 11:08:12 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './copmanets/navbar'
+import Content from './copmanets/context'
+export default function App() {
+  return (
+   <div>
+     <Navbar/>
+     <Content/>
+   </div>
+   
+
+  )
+}
---
+++ src/copmanets/navbar/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../navbar/style.css'
+export default function Navbar() {
+  return (
+    <div className='container'>
+      
+    </div>
+  )
+}
